5100dcd7f374266e889c3c29dcc0872482bc2d64
trquth <tranquthien@gmail.com>
2015-04-23 22:34:41 +0700

    HB8
    Start project

---
+++ HB8.CSMS.MVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace HB8.CSMS.MVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
