48aa24f269778f8b4c0566c13b8b32816453e423
trquth <tranquthien@gmail.com>
2015-04-30 23:44:28 +0700

    Fix
    Sua loi up hinh

--- HB8.CSMS.MVC/Controllers/StaffManagerController.cs
+++ HB8.CSMS.MVC/Controllers/StaffManagerController.cs
@@ -4,13 +4,15 @@ using HB8.CSMS.MVC.Models.Staff;
 using HB8.CSMS.MVC.Models.UploadImage;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.Mvc;
 
 namespace HB8.CSMS.MVC.Controllers
 {
-    public class StaffManagerController : Controller
+    public class StaffManagerController : UploadController
     {
         private IStaffManagerService<User> staffService;
         public StaffManagerController(IStaffManagerService<User> staffService)
@@ -18,9 +20,9 @@ namespace HB8.CSMS.MVC.Controllers
             this.staffService = staffService;
         }
         /// <summary>
-        /// Trả về một danh sách các chức vụ
+        /// Trả về một danh sách các chức vụ dang JSON
         /// </summary>
-        /// <returns></returns>
+        /// <returns>JSON</returns>
         public JsonResult ListPosition()
         {
             var model = staffService.GetListPosition();
@@ -32,36 +34,49 @@ namespace HB8.CSMS.MVC.Controllers
                         }).OrderBy(x => x.UserName);
             return Json(data, JsonRequestBehavior.AllowGet);
         }
+        /// <summary>
+        /// Trả về danh sách chức vụ 
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<StaffModel> GetListPosition()
+        {
+            var model = staffService.GetListPosition();
+            var data = (from a in model
+                        select new StaffModel
+                        {
+                            UserId = a.UserID,
+                            UserName = a.UserName,
+                        }).OrderBy(x => x.UserName);
+            return data;
+        }
         [HttpGet]
         public ViewResult CreateNewStaff()
         {
+            ViewBag.Position = GetListPosition();
             return View();
         }
         [HttpPost]
         public ViewResult CreateNewStaff(StaffModel staff)
         {
+            var newStaff = new Staff();
+            newStaff.StaffID = staff.ID;
+            newStaff.StaffName = staff.StaffName;
+            newStaff.Email = staff.Email;
+            newStaff.Address = staff.Address;
+            newStaff.NumberPhone = staff.NumberPhone;
+            newStaff.UserId = staff.UserId;
+            //newStaff.Image = SaveImage();
+            HttpPostedFileBase photo = Request.Files["fileupload"];
+            if (photo != null && photo.ContentLength > 0)
+            {
+                string extension = Path.GetExtension(photo.FileName);
+                var fileName = System.Guid.NewGuid().ToString("N") + extension;
+                var path = HostingEnvironment.MapPath("~/Images/");
+                photo.SaveAs(Path.Combine(path, fileName));
+                //return fileName;
+            }
             return View();
         }
-        [HttpPost]
-        public ContentResult UploadImage()
-        {
-            var imageName = new List<UploadImageModel>();
-            foreach (string file in Request.Files)
-            {
-                HttpPostedFileBase hpf = Request.Files[file] as HttpPostedFileBase;
-                if (hpf.ContentLength == 0)
-                {
-                    continue;
-                }
-                imageName.Add(new UploadImageModel()
-                {
-                    ImageName = hpf.FileName
-                });
-            }
-
-            return Content("{\"name\":\"" + imageName[0].ImageName + "\"}", "application/json");
-        }
-
 
     }
 }
---
+++ HB8.CSMS.MVC/Controllers/UploadController.cs
@@ -0,0 +1,52 @@
+﻿using HB8.CSMS.MVC.Models.UploadImage;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+
+namespace HB8.CSMS.MVC.Controllers
+{
+    public class UploadController : Controller
+    {
+        public ContentResult UploadImage()
+        {
+
+            var imageName = new List<UploadImageModel>();
+            foreach (string file in Request.Files)
+            {
+                HttpPostedFileBase hpf = Request.Files[file] as HttpPostedFileBase;
+                string savedFileName = Path.Combine(Server.MapPath("~/ImagesTemp"), Path.GetFileName(hpf.FileName));
+                hpf.SaveAs(savedFileName);
+
+                if (hpf.ContentLength == 0)
+                {
+                    continue;
+                }
+                imageName.Add(new UploadImageModel()
+                {
+                    ImageName = hpf.FileName
+                });
+            }
+
+            return Content("{\"name\":\"" + imageName[0].ImageName + "\"}", "application/json");
+        }
+        public string SaveImage()
+        {
+            HttpPostedFileBase photo = Request.Files["photo"];
+            if (photo != null && photo.ContentLength > 0)
+            {
+                string extension = Path.GetExtension(photo.FileName);
+                var fileName = System.Guid.NewGuid().ToString("N") + extension;
+                var path = HostingEnvironment.MapPath("~/Images/");
+                photo.SaveAs(Path.Combine(path, fileName));
+                return fileName;
+            }
+            return null;
+        }
+
+
+    }
+}
